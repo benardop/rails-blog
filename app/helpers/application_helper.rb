24d4a030e6d6a3df17e9225750038a6d64937266
benardop <benard.oc.p@gmail.com>
2023-01-02 14:41:18 +0300

    Add full_title helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return full title on page basis
+  def full_title(page_title = '')
+    base_title = 'Twitter clone Rails Sample App'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Twitter clone Rails Sample App"
+    assert_select "title", "Twitter clone Rails Sample App"
   end
 
   test "should get help" do
